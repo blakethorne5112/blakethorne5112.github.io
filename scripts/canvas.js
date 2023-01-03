f6da3a1ba1102c2d3b50c389ca3f9196a15fabf2
Jasper3671 <blake.thorne@hotmail.com>
2023-01-03 15:16:21 +1100

    Update Canvas / Skills
    Updated Canvas to a new feature that highlights boxes as you scroll over them. Also updated skills and profile information.
    
    TODO: Update Projects

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,68 +1,27 @@
-var space;
+const canvas = document.getElementById('pt')
+const colours = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 5000
 
-function floatySpace() {
-    var colors = [
-        "#FFFFFF"
-    ];
+for(let i = 0; i < SQUARES; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
 
-    space = new CanvasSpace("canvas", "#252934").display();
-    var form = new Form(space);
-
-    // Elements
-    var center = space.size.$divide(1.8);
-    var count = window.innerWidth * 0.05; // num of pt to create
-    if (count > 150) count = 150;
-    var mouse = center.clone();
-    var r = Math.min(space.size.x, space.size.y) * 1;
-    
-    var circleList = [];
-
-    for (var i = 0; i < count; i++) {
-        var circle = new Circle(Math.random() * Math.min(space.size.x), Math.random() * Math.min(space.size.y)).setRadius(1); // new point at random coords within screensize
-        circleList.push(circle); // Adds point p to array pts
-    }
-
-    var myCircles = {
-        animate: function (time, fs, context) {
-            for (var i = 0; i < count; i++) {
-                var current = circleList[i];
-
-                // form.fill("#FFFFFF").stroke(false).circle(current); // To set all circle colour 
-                form.circle(current);
-
-                var xDist = Math.abs(mouse.x - current.x);
-                var yDist = Math.abs(mouse.y - current.y);
-                var rDist = Math.sqrt((xDist * xDist) + (yDist * yDist));
-
-                if (rDist < 50) {                   
-                    form.fill("#ffd15f").stroke(false).circle(current);
-                    current.setRadius(3);
-
-                } else {
-                    form.fill("#FFFFFF").stroke(false).circle(current);
-                    current.setRadius(1);
-                }
-            }
-        },
-        onMouseAction: function (type, x, y, evt) {
-            if (type == "move") {
-                mouse.set(x, y);
-            }
-        },
-        onTouchAction: function (type, x, y, evt) {
-            this.onMouseAction(type, x, y);
-        }
-    };
-
-    space.add(myCircles)
-    space.bindMouse();
-    space.play();
+    square.addEventListener('mouseover', () => setColour(square))
+    square.addEventListener('mouseout', () => removeColour(square))
+    canvas.appendChild(square)
 }
 
-floatySpace();
+function setColour(element){
+    const colour = getRandomColour()
+    element.style.background = colour
+    element.style.boxShadow = `0 0 2px ${colour}, 0 0 10px ${colour}`
+}
 
-$(window).resize(function () {
-    space.removeAll();
-    $('canvas').remove();
-    floatySpace();
-});
+function removeColour(element){
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColour(){
+    return colours[Math.floor(Math.random() *colours.length)]
+}
